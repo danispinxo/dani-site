dcd41ebe9321aa5ab7fdbea01e6da6ff7c1c186e
Dani Spinosa <genericpronoun@gmail.com>
2024-04-29 19:48:02 -0400

    fix contact visuals

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -4,30 +4,36 @@ import Footer from '../components/Footer';
 import TextField from '@mui/material/TextField';
 
 const Contact = () => {
-  const [state, handleSubmit] = useForm('xkneyzbr');
+  const [state, handleSubmit] = useForm('xleqzjrj');
+  if (state.succeeded) {
+  }
 
   return (
     <>
       <TopNavbar />
+
       <main className="page-content">
         <div className="contact-title-holder">
           <p className="contact-title">Get In Touch</p>
         </div>
-
         <div className="contact-body">
-          <div className="contact-form">
-            <form onSubmit={handleSubmit} style={{ display: 'block' }}>
-              <TextField className="input" id="standard-basic" name="name" label="Name" variant="standard" required fullWidth />
-              <TextField className="input" id="standard-basic" name="email" label="Email" variant="standard" required fullWidth />
-              <ValidationError className="error" prefix="Email" field="email" errors={state.errors} />
-              <TextField className="input" id="outlined-multiline-static" name="message" label="Message" multiline rows={4} fullWidth />
-              <br />
-              <button className="form-submit-btn" type="submit" disabled={state.submitting}>
-                Send
-              </button>
-              <ValidationError className="error" errors={state.errors} />
-            </form>
-          </div>
+          {state.succeeded ? (
+            <p className="contact-message">Thanks for getting in touch!</p>
+          ) : (
+            <div className="contact-form">
+              <form onSubmit={handleSubmit} style={{ display: 'block' }}>
+                <TextField className="input" id="standard-basic" name="name" label="Name" variant="standard" required fullWidth />
+                <TextField className="input" id="standard-basic" name="email" label="Email" variant="standard" required fullWidth />
+                <ValidationError className="error" prefix="Email" field="email" errors={state.errors} />
+                <TextField className="input" id="outlined-multiline-static" name="message" label="Message" multiline rows={4} fullWidth />
+                <br />
+                <button className="form-submit-btn" type="submit" disabled={state.submitting}>
+                  Send
+                </button>
+                <ValidationError className="error" errors={state.errors} />
+              </form>
+            </div>
+          )}
         </div>
 
         <Footer />
