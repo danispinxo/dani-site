d07de6a3226c3b77a66ebf937cec3beecab29678
Dani Spinosa <genericpronoun@gmail.com>
2024-04-29 19:51:08 -0400

    add title to head

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -3,13 +3,17 @@ import Document, { Html, Head, Main, NextScript } from 'next/document';
 class MyDocument extends Document {
   render() {
     return (
-      <Html>
+      <Html lang="en">
         <Head>
+          <meta charset="UTF-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
             href="https://fonts.googleapis.com/css2?family=Cormorant:wght@500;600;700&family=Special+Elite&family=Varela+Round&display=swap"
             rel="stylesheet"
           />
+          <title>Dani Spinosa: Poet Dev && Dev Poet</title>
         </Head>
         <body>
           <Main />
