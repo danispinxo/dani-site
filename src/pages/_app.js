198235964aae4a4d7daac52c2801a208e0b85db8
Dani Spinosa <genericpronoun@gmail.com>
2024-08-06 15:46:32 -0400

    use charSet

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -15,9 +15,8 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta charset="UTF-8" />
+        <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Cormorant:wght@500;600;700&family=Special+Elite&family=Varela+Round&display=swap"
