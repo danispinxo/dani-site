934de6ab681f316a78a96d5583b88a76aa4102a6
Dani Spinosa <genericpronoun@gmail.com>
2024-08-26 13:25:27 -0400

    add favicon

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -18,6 +18,7 @@ function MyApp({ Component, pageProps }) {
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link rel="icon" href="/favicon.ico" />
         <link
           href="https://fonts.googleapis.com/css2?family=Cormorant:wght@500;600;700&family=Special+Elite&family=Varela+Round&display=swap"
           rel="stylesheet"
