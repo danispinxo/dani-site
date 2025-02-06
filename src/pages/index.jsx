b7f83b15d5e4905db4c4c1ee1c7065541a75061c
Dani Spinosa <genericpronoun@gmail.com>
2025-02-06 17:17:23 -0500

    add Canthius and links to bio

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -17,12 +17,21 @@ export default function Homepage() {
           <div className="bio-container">
             <h2 className="bio-heading">About</h2>
             <p>
-              Dani Spinosa is a poet, scholar, educator, writer, and a trained full-stack developer. She an adjunct professor, a software
-              developer at Hatch Coding, a digital and creative project manager, a co-founding editor of Gap Riot Press, the Managing Editor
-              of the Electronic Literature Directory, and the author of two books:
-              <i> OO: Typewriter Poems</i> (Invisible Publishing, 2020) and
-              <i>Anarchists in the Academy</i> (U of Alberta Press, 2018). She has published several chapbooks of poetry and several more
-              peer-reviewed journal articles on poetry. She lives in beautiful Wasaga Beach, Ontario.
+              Dani Spinosa is a poet, scholar, educator, writer, and a trained full-stack developer. She is an adjunct professor, a software
+              developer at Hatch Coding, a digital and creative project manager, a co-founding editor of{' '}
+              <a href="https://www.gapriotpress.com/" target="_blank" rel="noreferrer noopener">
+                Gap Riot Press
+              </a>
+              , President of the feminist literary journal,{' '}
+              <i>
+                <a href="https://www.canthius.com/" target="_blank" rel="noreferrer noopener">
+                  Canthius
+                </a>
+              </i>
+              , the Managing Editor of the Electronic Literature Directory, and the author of two books:
+              <i> OO: Typewriter Poems</i> (Invisible Publishing, 2020) and <i>Anarchists in the Academy</i> (U of Alberta Press, 2018). She
+              has published several chapbooks of poetry and several more peer-reviewed journal articles on poetry. She lives in beautiful
+              Wasaga Beach, Ontario.
             </p>
           </div>
         </div>
