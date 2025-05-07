0183aea30a55ad4b7eda10e19b5f659700356ef8
danispinxo <genericpronoun@gmail.com>
2025-05-07 12:24:36 -0400

    update back to list button action and look

--- src/components/ToDo/CompleteList.jsx
+++ src/components/ToDo/CompleteList.jsx
@@ -1,21 +1,20 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTableList } from '@fortawesome/free-solid-svg-icons';
+import { faRotateLeft } from '@fortawesome/free-solid-svg-icons';
 
 const CompleteList = ({ tasks, handleBackToList }) => {
+  const toggleButtonVisibility = (e) => {
+    e.currentTarget.classList.toggle('show-button');
+  };
+
   return (
     <div className="completed-tasks">
       <h2>Completed Tasks ({tasks.length})</h2>
       <ul className="completed-tasks-list">
         {tasks.map((t, index) => (
-          <li
-            className="completed-task"
-            key={index}
-            onMouseEnter={(e) => e.target.classList.add('show-button')}
-            onMouseLeave={(e) => e.target.classList.remove('show-button')}
-          >
+          <li className="completed-task" key={index} onClick={toggleButtonVisibility}>
             <span>{t.text}</span>
             <button className="back-to-list-button" onClick={() => handleBackToList(t)}>
-              <FontAwesomeIcon icon={faTableList} />
+              <FontAwesomeIcon icon={faRotateLeft} />
             </button>
           </li>
         ))}
