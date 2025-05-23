9f925bd7950ed23f8fdc8b69fa9d67e5e14559ef
danispinxo <genericpronoun@gmail.com>
2025-05-23 08:55:31 -0400

    Use col instead of flexbox for categories

--- src/components/ToDo/IncompleteList.jsx
+++ src/components/ToDo/IncompleteList.jsx
@@ -1,21 +1,30 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheck, faEllipsisVertical } from '@fortawesome/free-solid-svg-icons';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 const IncompleteList = ({ incompleteTasks, handleDeleteTask, handleMarkAsDone, handleEditTask }) => {
   const { categorizedTasks, uncategorizedTasks } = incompleteTasks;
   const list = Object.keys(categorizedTasks);
 
+  const shortenText = (text) => {
+    if (text.length > 20) {
+      return text.substring(0, 30) + '...';
+    }
+    return text;
+  };
+
   return (
-    <div className="d-flex justify-content-around">
+    <Row>
       {list &&
         list.map((category, index) => (
-          <div key={`category-${index}`}>
+          <Col key={`category-${index}`}>
             <h5>{category}</h5>
             <ol className="todo-list">
               {categorizedTasks[category].map((t, index) => (
                 <li key={index} className="todo-list-item">
                   <>
-                    <span>{t.text}</span>
+                    <span className="incomplete-task">{shortenText(t.text)}</span>
 
                     <button className="edit-task-button" onClick={() => handleEditTask(t)}>
                       <FontAwesomeIcon icon={faEllipsisVertical} />
@@ -28,21 +37,19 @@ const IncompleteList = ({ incompleteTasks, handleDeleteTask, handleMarkAsDone, h
                 </li>
               ))}
             </ol>
-          </div>
+          </Col>
         ))}
       {uncategorizedTasks.length > 0 && (
-        <div>
+        <Col>
           <h5>Uncategorized</h5>
           <ol className="todo-list">
             {uncategorizedTasks.map((t, index) => (
               <li key={index} className="todo-list-item">
                 <>
-                  <span>{t.text}</span>
-
+                  <span className="incomplete-task">{shortenText(t.text)}</span>
                   <button className="edit-task-button" onClick={() => handleEditTask(t)}>
                     <FontAwesomeIcon icon={faEllipsisVertical} />
                   </button>
-
                   <button className="finish-task-button" onClick={() => handleMarkAsDone(t)}>
                     <FontAwesomeIcon icon={faCheck} />
                   </button>
@@ -50,9 +57,9 @@ const IncompleteList = ({ incompleteTasks, handleDeleteTask, handleMarkAsDone, h
               </li>
             ))}
           </ol>
-        </div>
+        </Col>
       )}
-    </div>
+    </Row>
   );
 };
 
