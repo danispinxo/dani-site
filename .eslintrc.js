371205cfe3c16df9d33709c909b1f7cc7818a80f
danispinxo <genericpronoun@gmail.com>
2025-06-06 09:09:11 -0400

    Migrate ESLint configuration to .eslintrc.js format with enhanced React rules

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+import reactPlugin from "eslint-plugin-react";
+import nextPlugin from "@next/eslint-plugin-next";
+
+export default [
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    },
+    plugins: {
+      react: reactPlugin,
+      "@next/next": nextPlugin
+    },
+    rules: {
+      indent: ["error", 2],
+      "linebreak-style": ["error", "unix"],
+      quotes: ["error", "double"],
+      semi: ["error", "always"],
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+      "no-unused-vars": "warn",
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error",
+      "react/jsx-key": "error",
+      "react/jsx-no-duplicate-props": "error",
+      "react/jsx-no-undef": "error",
+      "react/jsx-uses-react": "error",
+      "react/jsx-uses-vars": "error",
+      "react/no-children-prop": "error",
+      "react/no-danger": "error",
+      "react/no-deprecated": "error",
+      "react/no-direct-mutation-state": "error",
+      "react/no-find-dom-node": "error",
+      "react/no-is-mounted": "error",
+      "react/no-render-return-value": "error",
+      "react/no-string-refs": "error",
+      "react/no-unknown-property": "error",
+      "react/require-render-return": "error",
+      "react/self-closing-comp": "error"
+    },
+    settings: {
+      react: {
+        version: "detect"
+      }
+    }
+  }
+];
