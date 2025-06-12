017cd3160bdb4760b88f3ddc649348315450877b
danispinxo <genericpronoun@gmail.com>
2025-06-12 08:13:32 -0400

    feat: add budget styles to the application

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -11,6 +11,7 @@ import "../styles/Publication.scss";
 import "../styles/When.scss";
 import "../styles/Swift.scss";
 import "../styles/ToDo.scss";
+import "../styles/budget.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { Cormorant, Special_Elite, Varela_Round } from "next/font/google";
