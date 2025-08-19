6f6a633bf7e4add64e56ecfcfe894e1179554bcd
danispinxo <genericpronoun@gmail.com>
2025-08-19 14:57:18 -0400

    add to port

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -6,6 +6,7 @@ import {
   faHeadphones,
   faLink,
 } from "@fortawesome/free-solid-svg-icons";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const determineIcon = (type) => {
   switch (type) {
@@ -20,7 +21,7 @@ const determineIcon = (type) => {
   }
 };
 
-const Book = ({ cover, title, press, date, notes, linkType, link }) => {
+const Book = ({ cover, title, press, date, notes, linkType, link, github }) => {
   return (
     <div className="book-component">
       <a href={cover} target="_blank" rel="noreferrer noopener">
@@ -36,6 +37,16 @@ const Book = ({ cover, title, press, date, notes, linkType, link }) => {
             rel="noreferrer noopener"
           >
             <FontAwesomeIcon icon={determineIcon(linkType)} />
+          </a>
+        )}
+        {github && (
+          <a
+            className="icon"
+            href={github}
+            target="_blank"
+            rel="noreferrer noopener"
+          >
+            <FontAwesomeIcon icon={faGithub} />
           </a>
         )}
         <i className="book-press">{press}</i>
--- src/pages/dev-portfolio.jsx
+++ src/pages/dev-portfolio.jsx
@@ -40,31 +40,56 @@ export default function Portfolio() {
               <div className="books-grid">
                 <Book
                   cover="/images/dani-site.png"
-                  title="www.danispinosa.dev"
-                  notes="Personal Website"
-                  link="https://github.com/danispinxo/dani-site"
+                  link="https://www.danispinosa.dev"
+                  title="Personal Website"
+                  github="https://github.com/danispinxo/dani-site"
                   linkType="devLink"
+                  date="2025"
+                  notes="This website showcases my portfolio, todo list app, and full CV."
                 />
                 <Book
                   cover="/images/jesse-site.png"
-                  title="www.jptherapystudios.com"
-                  notes="Jesse Pajuäär Therapy Studios Website"
-                  link="https://github.com/danispinxo/jesse-site"
+                  link="https://www.jptherapystudios.com"
+                  title="Jesse Pajuäär Therapy Studios Website"
+                  github="https://github.com/danispinxo/jesse-site"
                   linkType="devLink"
+                  date="2025"
+                  notes="A personal website for a therapist, featuring info and contact form."
+                />
+                <Book
+                  cover="/images/lexiforge.png"
+                  title="LexiForge"
+                  link="https://lexiforge.netlify.app/"
+                  github="https://github.com/danispinxo/lexiforge"
+                  linkType="devLink"
+                  date="2025"
+                  notes="React Rails app for creating constraint-based poems from open-source and public domain texts."
+                />
+                <Book
+                  cover="/images/compass.png"
+                  title="Compass by Lighthouse Labs"
+                  notes="Lighthouse Labs LMS"
+                  date="2023-2024"
+                />
+                <Book
+                  cover="/images/rudder.png"
+                  title="Rudder by Lighthouse Labs"
+                  notes="Lighthouse Labs Mentor Assistance Queue"
+                  date="2022-2024"
                 />
                 <Book
                   cover="/images/artify.png"
                   title="Artify"
-                  notes="Lighthouse Labs Final Project"
-                  link="https://github.com/danispinxo/artify"
-                  linkType="devLink"
+                  notes="Lighthouse Labs Final Group Project built in Express.js and React, website that allows artists to share and sell their work."
+                  github="https://github.com/danispinxo/artify"
+                  date="2022"
                 />
                 <Book
                   cover="/images/quizzes-mobile.png"
                   title="FAAQ U. Quizzes"
-                  notes="Lighthouse Labs Midterm Project"
-                  link="https://github.com/danispinxo/faaq-u-quizzes"
-                  linkType="devLink"
+                  notes="Lighthouse Labs Group Midterm Project, Express.js and EJS app that allows users to create and take quizzes."
+                  github="https://github.com/danispinxo/faaq-u-quizzes"
+                  date="2022"
                 />
               </div>
             </section>
