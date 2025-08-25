ff21dae4f81aeec97344c8f4a44978d14e5b7e03
danispinxo <genericpronoun@gmail.com>
2025-08-25 11:38:11 -0400

    handle fontawesome update

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: [
+    "@fortawesome/react-fontawesome",
+    "@fortawesome/fontawesome-svg-core",
+    "@fortawesome/free-solid-svg-icons",
+    "@fortawesome/free-brands-svg-icons",
+  ],
+};
+
+export default nextConfig;
