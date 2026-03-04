7eefee29fb1a20d0410ad2059ec7441543cb70a3
danispinxo <genericpronoun@gmail.com>
2026-03-04 10:22:11 -0500

    Update eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from "eslint/config";
-import reactPlugin from "eslint-plugin-react";
-import nextPlugin from "@next/eslint-plugin-next";
 
 export default defineConfig([
+  {
+    ignores: [".next/**", "node_modules/**", ".cache/**"],
+  },
   {
     files: ["**/*.{js,jsx}"],
     languageOptions: {
@@ -13,10 +14,6 @@ export default defineConfig([
           jsx: true,
         },
       },
-    },
-    plugins: {
-      react: reactPlugin,
-      "@next/next": nextPlugin,
     },
     rules: {
       indent: [
@@ -44,29 +41,6 @@ export default defineConfig([
       "react/prop-types": "off",
       "react/react-in-jsx-scope": "off",
       "no-unused-vars": "warn",
-      "react/jsx-uses-react": "error",
-      "react/jsx-uses-vars": "error",
-      "react/jsx-key": "error",
-      "react/jsx-no-duplicate-props": "error",
-      "react/jsx-no-undef": "error",
-      "react/jsx-uses-react": "error",
-      "react/jsx-uses-vars": "error",
-      "react/no-children-prop": "error",
-      "react/no-danger": "error",
-      "react/no-deprecated": "error",
-      "react/no-direct-mutation-state": "error",
-      "react/no-find-dom-node": "error",
-      "react/no-is-mounted": "error",
-      "react/no-render-return-value": "error",
-      "react/no-string-refs": "error",
-      "react/no-unknown-property": "error",
-      "react/require-render-return": "error",
-      "react/self-closing-comp": "error",
-    },
-    settings: {
-      react: {
-        version: "detect",
-      },
     },
   },
 ]);
