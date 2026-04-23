ea846c03ba4366b62338278c9a0fd89a6d32cd5e
danispinxo <genericpronoun@gmail.com>
2026-04-23 15:29:35 -0400

    Add OO: Typewriter Poems page with book details and praise quotes

---
+++ src/pages/creative-writing/oo-typewriter-poems.jsx
@@ -0,0 +1,281 @@
+import Book from "../../components/Book";
+import TopNavbar from "../../components/Navbar";
+
+const OOTypewriterPoems = () => {
+  return (
+    <>
+      <TopNavbar />
+      <div className="tech-grid" />
+      <main>
+        <div className="page-content">
+          <div className="page-container">
+            <h1 className="page-title">OO: Typewriter Poems</h1>
+
+            <section className="writing-section">
+              <Book
+                cover="/images/oo-cover.jpg"
+                title="OO: Typewriter Poems"
+                date="1 April 2020"
+                linkType="sale"
+                link="https://invisiblepublishing.com/product/oo-typewriter-poems/"
+              />
+
+              <div className="book-praise">
+                <h3 className="praise-heading">
+                  Praise for OO: Typewriter Poems
+                </h3>
+
+                <div className="praise-quote">
+                  <p>
+                    "I'm so glad this book exists—for what it means not only to
+                    me to read it now, but for what it will mean to others who
+                    are looking for a way into vispo that speaks to them and
+                    their lives. I love that it exists for the people who are
+                    going to be totally blown away by Spinosa and Siklosi's
+                    conversation, having never read anything quite like that
+                    before. This book is a real gift to vispo, its fans and
+                    present and future practitioners."
+                  </p>
+                  <cite>
+                    Helen Hajnoczky, <em>A Teacozy Is a Sometimes</em>
+                  </cite>
+                </div>
+
+                <div className="praise-quote">
+                  <p>
+                    "Dani Spinosa's latest book of poetry is a collection of
+                    glosas that is conceptually radical, formally daring, and
+                    stylistically wonderful—as smart and imaginative as anything
+                    one should by now expect of Spinosa."
+                  </p>
+                  <cite>
+                    Clayton Longstaff, <em>Canadian Literature</em>
+                  </cite>
+                </div>
+
+                <div className="praise-quote">
+                  <p>
+                    "The fifty typewriter poems in this volume are both thought
+                    provoking and beautifully crafted."
+                  </p>
+                  <cite>
+                    Jill Mandrake, <em>GEIST</em>
+                  </cite>
+                </div>
+
+                <div className="praise-quote">
+                  <p>
+                    "This collection is both Spinosa's personal study in the
+                    history of visual and concrete poetry as well as a
+                    collection of original works… She's clearly done her
+                    research, and if one were even to put together an anthology
+                    of or essay on the history of concrete and visual poetries,
+                    this would be the list of names included. Or, given
+                    Spinosa's deliberate inclusion of these multiple women
+                    practitioners, this is the list of names that should be
+                    included; and hopefully, in part through Spinosa's work, a
+                    list of names that will no longer be overlooked."
+                  </p>
+                  <cite>rob mclennan</cite>
+                </div>
+
+                <div className="praise-quote">
+                  <p>
+                    "Because the glosa, which builds a highly structured rhyming
+                    poem around a seed quatrain from a source text, is firmly
+                    rooted in the lyric, Spinosa has to find other ways to both
+                    quote and write through or around or over her sources. As
+                    she acknowledges in the introduction, even quoting a line
+                    from a concrete poem, much less an entire quatrain, can
+                    prove difficult as visual forms tend to challenge the
+                    convention of the line, but she more than manages this feat.
+                    The poems in the collection capture the aesthetics, the
+                    feel, of the texts they reference. Perhaps more importantly
+                    though, Spinosa's intervention in those texts unifies
+                    them.&nbsp;
+                    <em>OO</em>&nbsp;reads as a collection not an anthology with
+                    its poet, in the spirit of the glosa, paying tribute to
+                    influences and inspiration."
+                  </p>
+                  <cite>Ryan J. Cox, Prairie Fire</cite>
+                </div>
+
+                <div className="praise-quote">
+                  <p>
+                    "Dani Spinosa's OO pushes buttons, turns keys, and swipes,
+                    steals and homages all over poetry. Every poem demands you
+                    LOOK AGAIN! and see where voices slip between the
+                    keystrokes. Extravagant, interruptive, declarative and a
+                    real kick in the eyeballs."
+                  </p>
+                  <cite>derek beaulieu</cite>
+                </div>
+
+                <div className="praise-quote">
+                  <p>
+                    "WTF does Dani Spinosa think she is doing copying all these
+                    (mostly) male poets? Lock up your typewriters! Hide your
+                    anthologies of classic visual poetry! Protect yourself and
+                    the literary tradition from the stealth interventions of
+                    Spinosa, who is (mis)appropriating works by every
+                    conceivable author of graphically scored verse in the name
+                    of some kind of femmeship that involves conversations with
+                    the dead as well as the living. The former are silent on the
+                    matter and the latter? We shall see. Rarely has mimicry been
+                    used to such high-level hermeneutic ends."
+                  </p>
+                  <cite>Johanna Drucker</cite>
+                </div>
+
+                <div className="praise-quote">
+                  <p>
+                    "Not only an excellent, well-researched overview of the
+                    history and tradition of typewritten visual poetry, but
+                    also—what a sly female response to it!"
+                  </p>
+                  <cite>Petra Schulze-Wollgast</cite>
+                </div>
+
+                <h4 className="reviews-heading">
+                  Reviews of and Press for OO: Typewriter Poems
+                </h4>
+
+                <ul className="reviews-list">
+                  <li>
+                    Cox, Ryan J. "
+                    <a
+                      href="https://www.prairiefire.ca/oo-typewriter-poems-by-dani-spinosa/"
+                      target="_blank"
+                      rel="noreferrer noopener"
+                    >
+                      OO: Typewriter Poems by Dani Spinosa
+                    </a>
+                    ." <em>Prairie Fire</em>, 19 October 2021.
+                  </li>
+                  <li>
+                    Longstaff, Clayton. "
+                    <a
+                      href="https://canlit.ca/article/then-mars/"
+                      target="_blank"
+                      rel="noreferrer noopener"
+                    >
+                      Then, Mars
+                    </a>
+                    ." <em>Canadian Literature,</em> 1 September 2021.
+                  </li>
+                  <li>
+                    Heisler, Eva. "
+                    <a
+                      href="https://www.asymptotejournal.com/visual/typewriters-desire-community-dani-spinosa/"
+                      rel="noreferrer noopener"
+                    >
+                      Dani Spinosa, Typewriters, Desire, Community
+                    </a>
+                    ." <em>Asympote. </em>Article and interview.
+                  </li>
+                  <li>
+                    Desforges, Jaclyn. "
+                    <a
+                      href="http://hamiltonreviewofbooks.com/blog/2021/04/26/20-poetry-collections-you-may-have-missed-in-2020"
+                      rel="noreferrer noopener"
+                    >
+                      20 Poetry Collections You May Have Missed in 2020
+                    </a>
+                    ." <em>Hamilton Review of Books</em>, 2021
+                  </li>
+                  <li>
+                    mclennan, rob. "
+                    <a
+                      href="http://dusie.blogspot.com/2021/01/a-best-of-list-of-2020-canadian-poetry.html"
+                      rel="noreferrer noopener"
+                    >
+                      A 'best of' list of 2020 Canadian poetry books
+                    </a>
+                    ." <em>DUSIE</em>, 1 January 2020.
+                  </li>
+                  <li>
+                    Mandrake, Jill. "
+                    <a
+                      href="https://www.geist.com/fact/reviews/unity-order-equilibr/ium/"
+                      rel="noreferrer noopener"
+                    >
+                      Unity, Order, and Equilibr/ium
+                    </a>
+                    ." <em>GEIST</em>, vol. 116, 2020.
+                  </li>
+                  <li>
+                    Laliberte, Mark. "
+                    <a
+                      href="http://blog.carouselmagazine.ca/usereview003/"
+                      rel="noreferrer noopener"
+                    >
+                      USERVIEW 003: Creation, Derivation, Exchange."
+                    </a>
+                    <em>Carousel, </em>5 September.
+                  </li>
+                  <li>
+                    Earl, Amanda. "Episode 63 -- Interview with Dani Spinosa."
+                    <em>Small Machine Talks </em>(Podcast), 30 July.
+                  </li>
+                  <li>
+                    mclennan, rob. "
+                    <a
+                      href="http://robmclennan.blogspot.com/2020/06/dani-spinosa-oo-typewriter-poems.html"
+                      rel="noreferrer noopener"
+                    >
+                      Rev. of Dani Spinosa, <em>OO: Typewriter Poems</em>
+                    </a>
+                    ." 12 June.
+                  </li>
+                  <li>
+                    Hajnoczky, Helen. "
+                    <a
+                      href="https://www.ateacozyisasometimes.com/blog/2020/4/30/229mn1s4ah8xrzknyqlmupb2ila3kn"
+                      rel="noreferrer noopener"
+                    >
+                      OOH My Heart
+                    </a>
+                    ." <em>A Tea Cozy is a Sometimes</em>, 1 May.
+                  </li>
+                  <li>
+                    Beattie, Stephen. "In her new collection, Dani Spinosa
+                    locates visual poetry at the nexus of analogue and digital
+                    traditions."
+                    <em>Quill &amp; Quire</em>, 2 June.
+                  </li>
+                  <li>
+                    Beattie, Stephen. "<em>OO: Typewriter Poems</em>."
+                    <em>Quill &amp; Quire</em>, vol. 86, iss. 1, February 2020,
+                    p. 18.
+                  </li>
+                  <li>
+                    Ball, Jonathan. "
+                    <a
+                      href="https://www.winnipegfreepress.com/arts-and-life/entertainment/books/benaways-poetic-probing-resonates-569946152.html"
+                      rel="noreferrer noopener"
+                    >
+                      Rev. of <em>OO: Typewriter Poems</em>
+                    </a>
+                    <em>." Winnipeg Free Press</em>, 25 April.
+                  </li>
+                  <li>
+                    CBC Books. "
+                    <a
+                      href="https://www.cbc.ca/books/oo-typewriter-poems-1.5482144"
+                      rel="noreferrer noopener"
+                    >
+                      29 works of Canadian poetry to watch for in spring 2020
+                    </a>
+                    ." <em>CBC Books</em>, 2 March.
+                  </li>
+                </ul>
+              </div>
+            </section>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+};
+
+export default OOTypewriterPoems;
