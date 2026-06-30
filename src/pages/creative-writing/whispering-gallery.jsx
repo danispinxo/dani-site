e360b6905b31e50cb0d7d15658c7424502e09be9
danispinxo <genericpronoun@gmail.com>
2026-06-30 11:34:58 -0400

    Add publisher's weekly link

--- src/pages/creative-writing/whispering-gallery.jsx
+++ src/pages/creative-writing/whispering-gallery.jsx
@@ -18,6 +18,22 @@ const WhisperingGallery = () => {
                 date="forthcoming 13 October 2026"
                 notes="with Assembly Press"
               />
+
+              <div className="book-praise">
+                <h3 className="praise-heading">
+                  Praise for Whispering Gallery
+                </h3>
+
+                <h4 className="reviews-heading">
+                  Reviews of and Press for Whispering Gallery
+                </h4>
+
+                <ul className="reviews-list">
+                  <li>
+                    <i>Whisper Gallery</i> in the Publisher's Weekly <a href="https://www.publishersweekly.com/pw/by-topic/new-titles/adult-announcements/article/100723-fall-2026-fiction-nonfiction-preview-poetry.html" target="_blank" rel="noreferrer noopener">Fall 2026 Preview</a>
+                  </li>
+                </ul>
+              </div>
             </section>
           </div>
         </div>
